fac46a33f7fba16f039a2f0ad3efa584137314d4
oscarsmendez <oscarsmendez7@gmail.com>
2024-05-12 02:01:47 -0400

    changed more keybinds

--- config.def.h
+++ config.def.h
@@ -166,7 +166,7 @@ static const Key keys[] = {
 	/* Note that Shift changes certain key codes: c -> C, 2 -> at, etc. */
 	/* modifier                  key                 function        argument */
 	{ MODKEY,                    XKB_KEY_p,          spawn,          {.v = menucmd} },
-	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_n,     spawn,          {.v = termcmd} },
+	{ MODKEY,		     XKB_KEY_n,     spawn,          {.v = termcmd} },
 	{ MODKEY,                    XKB_KEY_j,          focusstack,     {.i = +1} },
 	{ MODKEY,                    XKB_KEY_k,          focusstack,     {.i = -1} },
 	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_J,          movestack,      {.i = +1} },
